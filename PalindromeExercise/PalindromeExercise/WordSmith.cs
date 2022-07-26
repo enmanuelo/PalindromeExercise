7dbb51cff93f7b86d1cff2a6900691c12cbc694b
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-26 11:21:32 -0400

    Created palindrome method and corresponding test

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public WordSmith()
+		{
+		}
+		public bool IsAPalindrome(string word)
+        {
+			string antiWord = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+				antiWord += word[i];
+            }
+            if (word == antiWord)
+            {
+				return true;
+            }
+			return false;
+        }
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("bib", true)]
+        [InlineData("mom", true)]
+        [InlineData("pam", false)]
+        [InlineData("dad", true)]
+        [InlineData("bad", false)]
+        public void Test1(string word, bool expected)
         {
-
+            //arrange
+            var smith = new WordSmith();
+            //act
+            var actual = smith.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
